f2ca141016731d81af6dff6720038285afb5df7e
tarekul42 <tarekulrifat205@gmail.com>
2023-09-19 20:37:01 -0700

    login and registration page design done

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import { Form } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div>
-            This is register section
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col">
+                <div className="text-center lg:text-left">
+                <h1 className="text-5xl font-bold">Register Now!</h1>
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                <Form className="card-body">
+                    <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Email</span>
+                    </label>
+                    <input type="text" placeholder="email" className="input input-bordered" required/>
+                    </div>
+                    <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Password</span>
+                    </label>
+                    <input type="password" placeholder="password" className="input input-bordered" required/>
+                    <label className="label">
+                        <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                    </label>
+                    </div>
+                    <div className="form-control mt-6">
+                    <button type="submit" className="btn btn-primary">Register</button>
+                    </div>
+                </Form>
+                </div>
+            </div>
         </div>
     );
 };
